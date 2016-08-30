0759b0b317a531ae9a4698f396700b9d30e4a453
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-08-30 16:14:28 +0200

    - check if glade file exists, if not, quit with an error - add some documentations

--- src/main.c
+++ src/main.c
@@ -4,18 +4,36 @@ int main(int argc, char *argv[])
 {
     GtkBuilder      *builder; 
     GtkWidget       *window;
- 
+    GError          *error;
+
+    /* Init GTK+ */ 
     gtk_init(&argc, &argv);
  
+    /* Create new GtkBuilder object */
     builder = gtk_builder_new();
-    gtk_builder_add_from_file (builder, "glade/window_main.glade", NULL);
+
+    /* Load UI from file. If error occurs, report it and quit application.
+     * Replace "tut.glade" with your saved project. */
+    if( !gtk_builder_add_from_file( builder, "glade/window_main.glade", &error ) )
+    {
+        g_warning("%s", error->message);
+        g_free(error);
+        return(1);
+    }
  
+    /* Get main window pointer from UI */
     window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
-    gtk_builder_connect_signals(builder, NULL);
+
+    /* Connect signals */
+    gtk_builder_connect_signals( builder, NULL );
  
-    g_object_unref(builder);
+    /* Destroy builder, since we don't need it anymore */
+    g_object_unref( G_OBJECT( builder ) );
  
-    gtk_widget_show(window);                
+    /* Show window. All other widgets are automatically shown by GtkBuilder */
+    gtk_widget_show( window );               
+
+    /* Start main loop */
     gtk_main();
  
     return 0;
